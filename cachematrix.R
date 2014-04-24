e53fdc255cd1b33af2ef96e93942812072869278
countonit <dennisjohn80@gmail.com>
2014-04-23 21:19:48 -0400

    My Function
    I added my function to the R file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions contained in the file allow a user to retrieve a cached inverse of a matrix 
+## rather than having to run the inverse function each time that R needs it. This is especially useful if the
+## inverse is needed many times like in a loop function.
 
-## Write a short comment describing this function
+## This function creates a vector that is used by the second function. It takes a matrix as an argument then
+## creates four functions.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## The second functions checks to see if we already have the inverse of the function in cache.
+## If so it will return that inverse from the cache as well as printing a message to the console
+## letting us know that it was able to retrieve it from cache, if not it will calculate the inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
+
+## This function is somewhat difficult to test but I ran the following to test 
+##it: cacheSolve(x<-makeCacheMatrix(matrix(1:4, ncol=2)))
